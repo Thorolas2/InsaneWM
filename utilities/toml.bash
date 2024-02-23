47612f45d5ebe46ac1f1656660757af7c6bfb10d
Thorolas2 <thorolas56@gmail.com>
2024-02-23 14:56:31 +0100

    Added configuration files for workspaces

--- utilities/toml.bash
+++ utilities/toml.bash
@@ -1,5 +1,10 @@
 
+# get_section(file)
+function get_section {
+    sed -r "s/^\[([a-zA-Z]+)\]((.|\n)*?)(?=\[[a-zA-Z]+\]|\z)/\1\n\2"
+}
+
 # get_property(file, section, property)
 function get_property {
-    :    
+    :
 }
